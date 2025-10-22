b21c04cb8006f5846f28f8602c33b4dd96f15731
David Nicolás <156855474+Dev-dns@users.noreply.github.com>
2025-10-22 19:37:50 +0200

    Add files via upload

---
+++ primer-curso/programacion/unidad-02-Java&Python/python/Triangulof.py
@@ -0,0 +1,10 @@
+altura = int(input("Inserte la altura de la piramide: "))
+texto = ""
+
+for i in range(altura,0, -1):
+	for j in range(0, altura-i):
+		texto+=" "
+	for j in range(2*i-1):
+		texto+="*"
+	texto+="\n"
+print(texto)
